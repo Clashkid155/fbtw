9f2cae87227fbc9817b42cac052b246effcf768c
fps <anthony.nnaemeka.umeh@gmail.com>
2023-01-19 23:09:39 +0100

    Test 2

---
+++ tests/T_test.py
@@ -0,0 +1,22 @@
+from brownie import accounts, reverts, T, config
+
+def owner():
+    return accounts[0]
+
+def deploy():
+    t = T.deploy({"from": owner()})
+    return t
+
+def test_fund():
+    t = deploy()
+    bal1 = owner().balance()
+
+    with reverts():
+        t.fund({"value": "6 ether", "from": owner()})
+    
+    t.fund({"value": "11 ether", "from": owner()})
+
+    bal2 = owner().balance()
+
+    assert bal1 - bal2 == "11 ether"
+    assert bal2 == "989 ether"
