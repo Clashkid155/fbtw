be7f4919c35d5efd351c8c425678eb8334713ea1
Clashkid155 <frankdiss9@gmail.com>
2023-01-20 12:50:13 +0100

    Update T.sol
    OmO

--- contracts/T.sol
+++ contracts/T.sol
@@ -3,6 +3,6 @@ pragma solidity ^0.8.14;
 
 contract T {
     function fund() public payable {
-        require(msg.value >= 10 ether, "Minimum fund of 10 Ether!");
+        require(msg.value >= 10 ether, "10 ether");
     }
 }
